c5a7eb291f5bd60a40450aaa800ade9f8cbefa21
笑年 <wochiliangwan@gmail.com>
2025-03-08 15:48:32 +0800

    创建mian.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void sort(int arr[], int size)
+{
+
+}
+
+int mian()
+{
+
+	int arr[] = { 12,4,89,43,32,78 };
+	int size = siezof(arr) / sizeof(arr[0]);
+	sort(arr, size);
+	return 0;
+}
